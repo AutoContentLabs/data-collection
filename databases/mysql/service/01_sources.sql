bf0c2468e71f4cf452c859243367c39e3aa864b3
Quenteon <QuanteonQuantumElysium@gmail.com>
2024-10-18 10:36:06 +0000

    feat : Database Configuration #3

--- databases/mysql/service/01_sources.sql
+++ databases/mysql/service/01_sources.sql
@@ -3,7 +3,7 @@ CREATE TABLE sources (
     source_id INT AUTO_INCREMENT PRIMARY KEY,
     source_name VARCHAR(255) NOT NULL UNIQUE,
     source_language ENUM('ENG', 'CHI' ) NOT NULL,
-    source_geo ENUM('Global', 'CHI' ) NOT NULL,
+    source_geo ENUM('Global', 'China' ) NOT NULL,
     source_type ENUM('Manual', 'Web' , 'RSS', 'API',  'Database' ) NOT NULL,
     access_method ENUM('GET', 'POST', 'WebSocket', 'FTP') NOT NULL,
     data_type ENUM('Text', 'CSV', 'XML', 'HTML', 'JSON' ) NOT NULL,
