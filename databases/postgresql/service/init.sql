1a72015d901be98b54c95322033dd4c0096d1add
Quenteon <QuanteonQuantumElysium@gmail.com>
2024-10-19 16:40:47 +0000

    dev: configure pgsql database model

--- databases/postgresql/service/init.sql
+++ databases/postgresql/service/init.sql
@@ -1,28 +1,45 @@
+-- API kaynaklı trend verileri
 CREATE TABLE api_trends (
   id SERIAL PRIMARY KEY,
+  -- API, Web, RSS, etc
   source_type VARCHAR(50),
+
+  -- GET, POST, WebSocket, FTP
   access_method VARCHAR(10),
+
+  -- JSON, XML, CSV, etc
   data_format VARCHAR(10),
+
+  -- JSON veya metin
   extracted_data JSON,
+
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Web scraping kaynaklı veriler
 CREATE TABLE web_trends (
   id SERIAL PRIMARY KEY,
   source_url TEXT,
+
+  -- HTML veya metin
   extracted_html TEXT,
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- RSS kaynaklı veriler
 CREATE TABLE rss_trends (
   id SERIAL PRIMARY KEY,
   rss_url TEXT,
+
+  -- metin
   extracted_data TEXT,
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Manuel girilen veriler
 CREATE TABLE manual_entries (
   id SERIAL PRIMARY KEY,
+  -- metin
   data_entry TEXT,
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
