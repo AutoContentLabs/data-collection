36abeff20bbe3f69b9864cc96096198af72d35a8
Quenteon <QuanteonQuantumElysium@gmail.com>
2024-10-19 16:39:56 +0000

    dev: configure mongodb database init

--- databases/mongodb/service/init.js
+++ databases/mongodb/service/init.js
@@ -1,7 +1,20 @@
-db = db.getSiblingDB('trends_db');  // Veritabanı oluştur veya seç
-db.createCollection('trend_data');  // Koleksiyon oluştur
+// MongoDB Bilgileri
+MONGO_DB_NAME = "trend_db";
 
-db.trend_data.insertMany([
-   { keyword: "Python", search_volume: 12345, collected_at: new Date() },
-   { keyword: "Docker", search_volume: 6789, collected_at: new Date() }
+db = db.getSiblingDB(MONGO_DB_NAME); // Veritabanı oluştur veya seç
+
+// api_data: API’lerden toplanan JSON verilerini depolayacak.
+db.createCollection("api_data");
+
+// web_data: Web scraping ile elde edilen verileri depolayacak.
+db.createCollection("web_data");
+
+// rss_data: RSS beslemelerinden toplanan veriler için.
+db.createCollection("rss_data");
+
+// manual_entries: Manuel olarak girilen veriler için.
+db.createCollection("manual_entries");
+
+db.manual_entries.insertMany([
+  { keyword: "test", search_volume: 12345, collected_at: new Date() },
 ]);
